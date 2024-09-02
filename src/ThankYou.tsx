43344e0e255618e18eb9ffa2fc74cfd5689b9187
Serkan Akman <akmanserkan65@gmail.com>
2024-09-02 22:54:27 +0300

    first commit

--- src/ThankYou.tsx
+++ src/ThankYou.tsx
@@ -5,9 +5,7 @@ const ThankYou: React.FC = () => {
     <div className="bg-blue-500 text-white p-8">
       <h1 className="text-3xl font-bold mb-4">Thank You</h1>
       <p className="text-lg">
-        Thank you for being a part of Navachar. Your contribution and presence are
-        invaluable to us. Together, we create a vibrant community dedicated to
-        positive change and informed discussions.
+        Thank you!
       </p>
     </div>
   );
